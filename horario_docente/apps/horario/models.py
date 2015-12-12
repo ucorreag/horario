40d579794f3c6dfa3b521afccfb30807c3e20f41
Joaquim <jnsakuv@vxp.com>
2015-12-12 01:03:54 -0500

    footer

--- horario_docente/apps/horario/models.py
+++ horario_docente/apps/horario/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
 class Curso(models.Model):
-<<<<<<< HEAD
     nombre = models.CharField(max_length=25)
     
     def __str__(self):
         return self.nombre
-=======
-    nombre = models.CharField(max_length=25)
->>>>>>> a6d2dba... Modelos
